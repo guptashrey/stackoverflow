16c0495452642ac4b101276f91e6c312a45f837c
Shrey Gupta <shrey.gupta@zs.com>
2022-01-22 16:01:06 +0530

    Fixed typos

--- Salary_ML.py
+++ Salary_ML.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 def clean_data(df):
